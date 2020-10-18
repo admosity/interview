0192a059da13c058cb38878c9147df2e1622d8b4
Adam Lee <duo1123@gmail.com>
2020-10-18 18:53:40 -0400

    feat: split out connections

--- config/database.js
+++ config/database.js
@@ -1,15 +1,53 @@
-module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+
+const { ConnectionString } = require('connection-string');
+
+
+
+
+module.exports = ({ env }) => {
+  const databaseUrl = env('DATABASE_URL');
+
+  if (databaseUrl) {
+    const connectionString = new ConnectionString(databaseUrl);
+
+    const connectionParsed = {
+      host: connectionString.hostname,
+      port: connectionString.port,
+      database: connectionString.path?.[0],
+      username: connectionString.user,
+      password: connectionString.password,
+      ssl: connectionString.params?.ssl ? Boolean(connectionString.params.ssl) : undefined,
+    };
+
+    return {
+      defaultConnection: 'default',
+      connections: {
+        default: {
+          connector: 'bookshelf',
+          settings: {
+            client: 'postgres',
+            ...connectionParsed,
+          },
+          options: {}
+        },
       },
-      options: {
-        useNullAsDefault: true,
+    };
+  }
+
+  // use sqlite locally
+  return {
+    defaultConnection: 'default',
+    connections: {
+      default: {
+        connector: 'bookshelf',
+        settings: {
+          client: 'sqlite',
+          filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        },
+        options: {
+          useNullAsDefault: true,
+        },
       },
     },
-  },
-});
+  };
+};
