88d4b2903316075e06d9a40ef5ae0ea041d5c832
Adam Lee <duo1123@gmail.com>
2020-10-18 17:23:39 -0400

    feat: initial

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'dc006b91ec6b4700083715e9de8739a2'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '02d6478b-1941-4533-a68c-dafc76b427bc'
+};
