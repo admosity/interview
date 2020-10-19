e8bc80e552d23533e5bd150abbc5d1236695c84a
Adam Lee <duo1123@gmail.com>
2020-10-19 09:48:14 -0400

    fix: add methods

--- config/middleware.js
+++ config/middleware.js
@@ -1,11 +1,10 @@
-module.exports = ({
-  env
-}) => {
+module.exports = ({ env }) => {
   return {
     settings: {
       cors: {
         enabled: true,
-      }
-    }
-  }
-}
+        methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS', 'HEAD'],
+      },
+    },
+  };
+};
